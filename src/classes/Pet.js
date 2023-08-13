4cab82d7bfd138b342996efcac2fbc2557c56ae0
Nadir <2266923@bdeb.qc.ca>
2023-08-13 16:40:54 -0400

    handle pet orientation

--- src/classes/Pet.js
+++ src/classes/Pet.js
@@ -24,25 +24,95 @@ export default class Pet extends Phaser.Physics.Matter.Sprite {
 		this.setFixedRotation();
 		this.setBounce(1);
 		this.setCollisionGroup(-1);
-		this.animate();
+		this.animationKeys = ["0", "45", "90", "135", "180", "225", "270", "315"];
+		this.addAnimations();
 		this.makeFleeMouse();
 	}
 
-	animate() {
+	addAnimations() {
 		this.anims.create({
-			key: "pet-anim",
-			frames: this.anims.generateFrameNames("pets-atlas", {
-				start: 1,
-				end: 8,
-				zeroPad: 0,
-				prefix: this.color + "/",
-				suffix: ".png",
-			}),
+			key: "90",
+			frames: [
+				{
+					key: "pets-atlas",
+					frame: this.color + "/1.png",
+				},
+			],
+		});
+
+		this.anims.create({
+			key: "135",
+			frames: [
+				{
+					key: "pets-atlas",
+					frame: this.color + "/2.png",
+				},
+			],
+		});
+
+		this.anims.create({
+			key: "180",
+			frames: [
+				{
+					key: "pets-atlas",
+					frame: this.color + "/3.png",
+				},
+			],
+		});
+
+		this.anims.create({
+			key: "225",
+			frames: [
+				{
+					key: "pets-atlas",
+					frame: this.color + "/4.png",
+				},
+			],
+		});
+
+		this.anims.create({
+			key: "270",
+			frames: [
+				{
+					key: "pets-atlas",
+					frame: this.color + "/5.png",
+				},
+			],
+		});
+
+		this.anims.create({
+			key: "315",
+			frames: [
+				{
+					key: "pets-atlas",
+					frame: this.color + "/6.png",
+				},
+			],
+		});
+
+		this.anims.create({
+			key: "0",
+			frames: [
+				{
+					key: "pets-atlas",
+					frame: this.color + "/7.png",
+				},
+			],
+		});
+
+		this.anims.create({
+			key: "45",
+			frames: [
+				{
+					key: "pets-atlas",
+					frame: this.color + "/8.png",
+				},
+			],
 			repeat: -1,
 			duration: 1000,
 		});
 
-		this.play("pet-anim");
+		this.play("90");
 	}
 
 	makeFleeMouse() {
@@ -73,7 +143,52 @@ export default class Pet extends Phaser.Physics.Matter.Sprite {
 				const newPosX = this.x + deplacementX;
 				const newPosY = this.y + deplacementY;
 				this.setPosition(newPosX, newPosY);
+
+				const suitableAngle = this.findSuitableAngle(
+					deplacementX,
+					deplacementY
+				);
+				this.play(suitableAngle);
 			}
 		});
+	}
+
+	findSuitableAngle(deplacementX, deplacementY) {
+		// Calculate the angle using arctangent (atan2) function
+		const angleRadians = Math.atan2(deplacementY, deplacementX);
+
+		// Convert radians to degrees and ensure the angle is positive
+		let angleDegrees = (angleRadians * 180) / Math.PI;
+		if (angleDegrees < 0) {
+			angleDegrees += 360;
+		}
+
+		// Array of available angles
+		const availableAngles = [
+			"0",
+			"45",
+			"90",
+			"135",
+			"180",
+			"225",
+			"270",
+			"315",
+		];
+
+		// Find the closest angle from the array
+		let closestAngle = availableAngles[0];
+		let minAngleDifference = Math.abs(angleDegrees - parseInt(closestAngle));
+
+		for (let i = 1; i < availableAngles.length; i++) {
+			const currentAngle = parseInt(availableAngles[i]);
+			const angleDifference = Math.abs(angleDegrees - currentAngle);
+
+			if (angleDifference < minAngleDifference) {
+				minAngleDifference = angleDifference;
+				closestAngle = availableAngles[i];
+			}
+		}
+
+		return closestAngle;
 	}
 }
